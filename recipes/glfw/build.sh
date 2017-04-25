251ce59f7d989f04dfb6a216c237b18161c97986
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2017-04-25 12:04:17 -0500

    Fix tests

--- recipes/glfw/build.sh
+++ recipes/glfw/build.sh
@@ -16,7 +16,7 @@ make && make install
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   install_name_tool -id @rpath/libglfw.3.dylib $PREFIX/lib/libglfw.3.2.dylib
-  DYLD_LIBRARY_PATH="$PREFIX/lib" DISPLAY=":99.0" tests/glfwinfo
+  DISPLAY=":99.0" tests/glfwinfo
 elif [[ "$OSTYPE" == "linux"* ]]; then
   LD_LIBRARY_PATH="$PREFIX/lib" DISPLAY=":99.0" tests/glfwinfo
 fi
